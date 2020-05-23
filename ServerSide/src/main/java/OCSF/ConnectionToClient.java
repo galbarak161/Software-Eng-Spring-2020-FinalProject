296487303037a1100b2c9d7f6ddf73395c156f51
Gal Barak <Gal@DESKTOP-BEFQF8M>
2020-05-23 13:37:34 +0300

    Update project framework according to prototype

--- ServerSide/src/main/java/OCSF/ConnectionToClient.java
+++ ServerSide/src/main/java/OCSF/ConnectionToClient.java
@@ -157,7 +157,7 @@ public class ConnectionToClient extends Thread
   {
     if (clientSocket == null || output == null)
       throw new SocketException("socket does not exist");
-
+    output.reset();
     output.writeObject(msg);
   }
 
