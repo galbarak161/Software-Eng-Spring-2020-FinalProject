8848b0c45841d4bfb24279f843d216b4a7fdc0f8
sagigvili <57597612+sagigvili@users.noreply.github.com>
2020-06-22 10:15:49 +0300

    Change icon

--- ClientSide/src/main/java/Client/showStudentTest.java
+++ ClientSide/src/main/java/Client/showStudentTest.java
@@ -1,7 +1,6 @@
 package Client;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
@@ -12,10 +11,8 @@ import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.greenrobot.eventbus.EventBus;
 
 import CloneEntities.CloneAnswerToQuestion;
-import CloneEntities.CloneExam;
 import CloneEntities.CloneQuestionInExam;
 import CloneEntities.CloneStudentTest;
-import CloneEntities.CloneTest;
 import CloneEntities.CloneTest.ExamType;
 import CloneEntities.CloneTest.TestStatus;
 import CloneEntities.CloneUser.UserType;
@@ -25,18 +22,13 @@ import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.Orientation;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Separator;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 
